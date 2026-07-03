20bfd265e962c7361e844cb18449ce400b79b723
harini <samaharininaresh@gmail.com>
2026-07-03 13:43:40 +0530

    update port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app=express()
 
-const PORT=3002;
+const PORT=process.env.PORT ||  3002;
 
 app.listen(PORT, function(){
     console.log("sucessfully running at http://localhost:"+PORT)
